f6fd68574ad8a3c0687e2676903b703a9a433e81
Palle Westermann <pwe@tec.dk>
2025-10-29 10:13:00 +0100

    RoutingCloseToDone

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'login', component:LoginComponent},
+  {path:'about', component:AboutComponent},
+  {path:'contact', component:ContactComponent},
+  {path:'', component:HomeComponent},
+  {path:'**', component:PageNotFoundComponent},
+];
