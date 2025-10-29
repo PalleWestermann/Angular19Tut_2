186be3cc7ffd9d9259bc8329cdfa1e26ee764510
Palle Westermann <pwe@tec.dk>
2025-10-29 10:28:09 +0100

    AddedLinkSourceEtc

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,4 +10,32 @@ import { HeaderComponent } from "./header/header.component";
 })
 export class App {
   protected readonly title = signal('Angular19Tut_2');
+
+  users = [
+    {
+      id: '1',
+      name: 'Anil',
+      age:28
+    },
+        {
+      id: '2',
+      name: 'Sam',
+      age:29
+    },
+        {
+      id: '3',
+      name: 'Bruce',
+      age:31
+    },
+        {
+      id: '3',
+      name: 'Peter',
+      age:23
+    },
+        {
+      id: '5',
+      name: 'Tony',
+      age:45
+    },
+  ];
 }
